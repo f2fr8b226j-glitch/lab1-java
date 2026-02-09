afbd2d45deff2e58dbe53dd3b2c690f89715fbad
Aliya <f2fr8b226j@privaterelay.appleid.com>
2026-02-09 09:11:49 +0500

    Add files via upload

---
+++ lab1/Main.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class Main {
+
+    // Точка входа в программу
+    public static void main(String[] args) {
+
+        // Вывод информации о студенте
+        System.out.println("ФИО: Иванов Иван Иванович");
+        System.out.println("Группа: ИС-21");
+        System.out.println("Специальность: Информационные системы");
+
+    }
+
+}
