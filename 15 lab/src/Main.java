55d76f5fcf9c74126a0ada2a3e9bacfc8d44e155
Aliya <f2fr8b226j@privaterelay.appleid.com>
2026-04-27 09:57:37 +0500

    Add files via upload

---
+++ 15 lab/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        int difference = a - b;
+
+        System.out.println(difference);
+    }
+}
